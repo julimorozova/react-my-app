f0ef19303bb4defed82c9a16e4a1f6b1f0687733
julimorozova <yuli.sleptsova@gmail.com>
2021-10-28 11:18:41 +0300

    code refactoring: add switch

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -2,20 +2,21 @@ const ADD_MESSAGE = 'ADD-MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
 export const dialogsReducer = (state, action) => {
+    switch (action.type) {
+        case ADD_MESSAGE:
+            let newMessage = {
+                id: 4,
+                message: state.newMessageText
+            }
+            state.messagesData.push(newMessage);
+            state.newMessageText = '';
+            return state;
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageText = action.newText;
+            return state;
+        default:
+            return state;
 
-
-    if (action.type === ADD_MESSAGE) {
-        let newMessage = {
-            id: 4,
-            message: state.newMessageText
-        }
-        state.messagesData.push(newMessage);
-        state.newMessageText = '';
-
-    } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
-        state.newMessageText = action.newText;
     }
-
-    return state;
 }
 export default dialogsReducer;
